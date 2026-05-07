a5a7a8372b72f9dcf27484278119fb8cfc907525
Subhankar <maitisubhankar55@gmail.com>
2026-05-07 10:37:03 +0530

    db designing

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose,{Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+        },
+        avatar:{
+            type:String, //cloudnary url
+            required:true,
+        },
+        coverImage:{
+            type:String, //cloudnary url
+        },
+        
+    }
+
+)
+
+
+export const User = mongoose.model("User",userSchema);
