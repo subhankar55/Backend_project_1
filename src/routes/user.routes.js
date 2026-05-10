58db05ba5ee358b9c48f147e677094150907a2f1
Subhankar <maitisubhankar55@gmail.com>
2026-05-10 18:17:16 +0530

    login and logut done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import registerUser from "../controllers/user.controller.js";
+import {loginUser, registerUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -18,5 +19,13 @@ router.route("/register").post(
     ]),
     registerUser
 );
+
+router.route("/login").post(loginUser);
+
+//secure routes
+
+router.route("/logout").post(verifyJWT,logoutUser);
+
+
 
 export default router;
