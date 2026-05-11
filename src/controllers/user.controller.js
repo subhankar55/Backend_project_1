66d12c59868677ef7a398b396b72e1b1315df2bb
Subhankar <maitisubhankar55@gmail.com>
2026-05-11 14:27:21 +0530

    nested lookups in mongo db implemented

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -420,6 +420,59 @@ const getUserChannelProfile = asyncHandler(
 
 );
 
+const getWatchHistory = asyncHandler(
+    async (req,res) => {
+        const user = await User.aggregate([
+            {
+                $match:{
+                    _id: new mongoose.Types.ObjectId(req.user._id)
+                
+                
+                }
+            },
+            {
+                $lookup:{
+                    from:"videos",
+                    localField:"watchHistory",
+                    foreignField:"_id",
+                    as:"watchHistory",
+                    pipeline:[
+                        {
+                            $lookup:{
+                                from:"users",
+                                localField:"owner",
+                                foreignField:"_id",
+                                as:"owner",
+                                pipeline:[
+                                    {
+                                        $project:{
+                                            fullname:1,
+                                            username:1,
+                                            avatar:1
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            $addFields:{
+                                owner:{
+                                    $first: "$owner"
+                                }
+                            }
+                        }
+                    ]
+                            
+                }
+            }
+        ])
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully")
+        )
+    }
+)
 
 
 export {
@@ -432,8 +485,7 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUsercoverImage,
-    getUserChannelProfile
-
-
+    getUserChannelProfile,
+    getWatchHistory
 
 };
